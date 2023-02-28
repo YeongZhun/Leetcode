f9d2d669256475479638fb60f0d8f1c29199e962
YeongZhun <yeongzhun.work@gmail.com>
2023-02-28 16:05:13 +0800

    Typo

--- 110. Balanced Binary Tree.py
+++ 110. Balanced Binary Tree.py
@@ -9,7 +9,19 @@ class TreeNode(object):
 
 class Solution(object):
     def isBalanced(self, root: TreeNode) -> bool:
-        return
+        
+        def dfs(root):
+            if not root: return [True, 0]
+
+            left, right = dfs(root.left), dfs(root.right)
+            balanced = (left[0] and right[0] and 
+                        abs(left[1] - right[1]) <= 1)
+            
+            return [balanced, 1 + max(left[1], right[1])]
+        
+        return dfs(root)[0]
+
+
     #Print the binary tree in in-order traversal
     def inOrderTraversal(self, Node: TreeNode):
         if (Node == None):
